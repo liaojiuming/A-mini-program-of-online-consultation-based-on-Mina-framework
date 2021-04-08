20f69059a1db3c80d921caeccb4ad4e4730b0ffe
liaojiuming <1185128484@qq.com>
2021-04-08 23:15:46 +0800

    Initial Commit

---
+++ miniprogram/pages/main/main.js
@@ -0,0 +1,41 @@
+Page({
+  onShareAppMessage() {
+    return {
+      title: 'swiper',
+      path: 'pages/main/main'
+    }
+  },
+
+  data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 500
+  },
+
+  changeIndicatorDots() {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+
+  changeAutoplay() {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+
+  intervalChange(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+
+  durationChange(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  }
+})
